875c794daf60b0b7018a7c2280bd94fcf8e73629
Manu <93039866+manu-nr@users.noreply.github.com>
2025-12-13 00:19:09 +0530

    Implemented player movement and animations

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float _movementSpeed = 10f;
+    [SerializeField] private float _rotationSpeed = 0.05f;
+    [SerializeField] private float _attackCooldownTime = 2.5f;
+
+
+    private CharacterController _characterController;
+    private Animator _animator;
+    private bool _isAttacking;
+
+    #region Unity Methods
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        HandlePlayerMovement();
+        HandlePlayerAttack();
+    }
+    #endregion
+
+    #region Private Methods
+    private void HandlePlayerAttack()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (!_isAttacking)
+            {
+                Attack();
+            }
+        }
+    }    
+    private void HandlePlayerMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+
+        bool isMoving = movement != Vector3.zero ? true : false;
+
+        if(isMoving && !_isAttacking)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed);
+            _characterController.Move(movement.normalized * _movementSpeed * Time.deltaTime);
+
+            SetRunAnimation();
+        }
+        else
+        {
+            if(_animator.GetBool("IsRunning") && !_isAttacking)
+                SetIdleAnimation();
+        }
+    }
+
+    private void Attack()
+    {
+        SetAttackAnimation();
+        StartCoroutine(SetIsAttacking());
+    }
+
+    private void SetAttackAnimation()
+    {
+        _animator.SetTrigger("Attack");
+    }
+
+    private void SetRunAnimation()
+    {
+        _animator.SetBool("IsRunning", true);
+    }
+
+    private void SetIdleAnimation()
+    {
+        _animator.SetBool("IsRunning", false);
+    }
+    #endregion
+
+    #region Coroutines
+    private IEnumerator SetIsAttacking()
+    {
+        _isAttacking = true;
+        yield return new WaitForSeconds(_attackCooldownTime);
+        _isAttacking = false;
+    }
+    #endregion
+}
